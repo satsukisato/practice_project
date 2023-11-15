99ccf057ded1571ffdfa3311a7bcfaee67a90841
satsuki sato <timothee0034@gmail.com>
2023-11-15 16:58:14 +0900

    first commit

---
+++ src/main/java/practice/Sample.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Sampleです");
+		System.out.println("java");
+	}
+
+}
